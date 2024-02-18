728c07de9174f2fa4b58fa40185a0af8fc7e4916
wwMalcolm <100085320+wwMalcolm@users.noreply.github.com>
2024-02-18 16:51:19 +0800

    Finished 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,41 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0 && year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+
+// let value = 1997;
+// if (value % 4 == 0 && (value % 100 != 0 && value % 400 == 0)) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+// if (value % 4 == 0 && value % 400 == 0) {
+//     console.log("true");
+// } else if (value % 4 == 0 && value % 100 == 0) {
+//     console.log("false");
+// } else if (value % 4 == 0) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+// if ((value % 4 == 0 && value % 400 == 0) || value % 4 == 0) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
 
 // Do not edit below this line
 module.exports = leapYears;
